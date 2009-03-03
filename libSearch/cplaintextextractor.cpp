9c0da3042df52f2952d6919d91e8d16ebd80f646
Parshin Dmitry <parshin@corp.mail.ru>
2009-03-03 21:27:48 +0300

    in progresS

--- libSearch/cplaintextextractor.cpp
+++ libSearch/cplaintextextractor.cpp
@@ -3,3 +3,8 @@
 CPlainTextExtractor::CPlainTextExtractor()
 {
 }
+
+ boost::signal1< void, const std::string& >& CPlainTextExtractor::SigDataObtained()
+ {
+     returm m_sigDataObtained;
+ }
--- libSearch/cplaintextextractor.h
+++ libSearch/cplaintextextractor.h
@@ -8,6 +8,20 @@ public:
     CPlainTextExtractor();
 
     boost::signal1< void, const std::string& >& SigDataObtained();
+
+private:
+
+    enum enumEvents
+    {
+        EV_FILEAVAIL,
+        EV_STOP
+    };
+
+    void ReleaseWait( int iAction
+
+    void OnSignal( const std::string& strFileName );
+
+    boost::signal1< void, const std::string& > m_sigDataObtained;
 };
 
 #endif // CPLAINTEXTEXTRACTOR_H
