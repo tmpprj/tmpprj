13d1ce85ce821bcd43ce0abb140ab53c46a0bd8f
Eugene Bolotin <bolotin@corp.mail.ru>
2009-06-25 11:55:50 +0400

    common added

---
+++ libCommon/src/common.cc
@@ -0,0 +1,10 @@
+#include "common.h"
+
+QString GetFileExtension( const QString& strFileName )
+{
+    int nPos = strFileName.lastIndexOf( '.' );
+    if( nPos == -1 )
+        return "";
+
+    return strFileName.mid( nPos + 1 );
+}
