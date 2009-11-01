c2f1285b79f47d7b56af81dc88ea1b0c95b2c9c9
unknown <_@.(none)>
2009-11-02 00:18:01 +0300

    FileSearcher: symlink handle

--- libSearch/include/filesearcher.h
+++ libSearch/include/filesearcher.h
@@ -8,6 +8,7 @@
 #include <memory>
 #include <QString>
 #include <QStringList>
+#include <QRegExp>
 
 #include "datahandler.hpp"
 
@@ -28,6 +29,7 @@ class CFileSearcher: public CDataHandler< FileSearcher::structParams >
 {
     boost::signals2::signal1< void , const QString& > m_sigFileFound;
     boost::signals2::signal0< void > m_sigDone;
+	std::vector< QRegExp > m_Filters;
 
     void Search( const QString& strPath, const QStringList& listMasks, bool bRecursive, size_t stMinFileSize, size_t stMaxFileSize, bool bSigDone );
 
